f6dbf6bc966ffe45e68e2b8018499fde35ba25fb
Keith Bourgoin <keith.bourgoin@gmail.com>
2022-11-23 21:19:16 +0000

    Added a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 #![feature(test)]
+
+use clap::Parser;
+
 mod collatz;
 
+/// collatz -- run the 3x+1 problem
+#[derive(Parser)]
+struct Args {
+    /// Start running at N
+    #[clap(short, long, default_value_t=1)]
+    start: usize,
+    /// Run for i numbers (0 runs forever)
+    #[clap(short, long, default_value_t=0)]
+    count: usize,
+}
+
 fn main() {
-    // println!("Found in: {}", collatz::recursive(27));
-    for n in 1..10 {
-        let result = collatz::shortcut(n);
-        println!("{}: {}", n, result);
+    let args = Args::parse();
+    let count_msg: String;
+    if args.count == 0 {
+        count_msg = "all".to_string();
+    } else {
+        count_msg = format!("{}", args.count);
+    }
+    println!("Running for {} numbers starting at {}", count_msg, args.start);
+
+    let mut i = 0;
+    while args.count == 0 || i < args.count {
+        let result = collatz::shortcut(args.start + i);
+        // Print every million so we saturate CPU
+        if (i+1) % 1_000_000 == 0 {
+            println!("{:e}: {}", args.start+i, result);
+        }
+        i += 1;
     }
 }
